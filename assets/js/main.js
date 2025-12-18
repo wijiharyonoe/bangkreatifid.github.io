54740592bd8c15414d6bd5d134c6b565c8ec31f4
wijiharyonoe <151423314+wijiharyonoe@users.noreply.github.com>
2025-12-18 20:48:02 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
 <script src="assets/js/main.js"></script>
+
+document.getElementById("search").addEventListener("keyup", function(){
+  let q = this.value.toLowerCase();
+  document.querySelectorAll(".mod-card").forEach(card=>{
+    card.style.display =
+      card.innerText.toLowerCase().includes(q) ? "block" : "none";
+  });
+});
